313723f810e4e6864b4f94469a4998d456a5e7c1
vmunoz-smartcosta <vmunoz@smartcosta.com>
2026-01-16 09:34:38 -0500

    chore: scaffold angular project structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  // Las rutas se cargarán dinámicamente en los siguientes pasos
+];
